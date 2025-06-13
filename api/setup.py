954980495e3d0be5b2dc52a4e348e8c88dab2bef
googer_ <googer_@googer.xyz>
2025-06-13 22:25:50 +0700

    api: add setup.py

---
+++ api/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+   name="scalar",
+   version="1.0",
+   license="MIT",
+
+   description='Scalar API implementation',
+   long_description=long_description,
+
+   author='googer_',
+   url="https://github.com/Def-Try/scalar",
+
+   packages=['scalar',
+             'scalar.protocol',
+             'scalar.protocol.encryption',
+             'scalar.protocol.packets',
+             'scalar.protocol.socket',
+             'scalar.server',
+             'scalar.client'],
+   install_requires=['cryptography'],
+)
