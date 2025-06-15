4a10ed3e09a653f8b154f4ce8991aa311f0ac31c
googer_ <googer_@googer.xyz>
2025-06-16 00:10:33 +0700

    scalar0: scalar0 client implementation

---
+++ api/scalar/client/implementations/scalar0.py
@@ -0,0 +1,42 @@
+from scalar.client.baseclient import BaseClient
+import scalar.primitives as primitives
+import scalar.protocol.packets.protocol as protocol
+
+class Scalar0Client(BaseClient):
+    _userlist: list[primitives.User] = []
+    async def event_on_login_completed(self):
+        await self._send_packet(protocol.SERVERBOUND_UserListRequest)
+        for packet in await self.recv_packets():
+            if type(packet) is not protocol.CLIENTBOUND_UserListResponse:
+                await self._process_packet(type(packet), packet)
+                continue
+            self._userlist = packet.users
+
+    async def _process_packet(self, packet_type: type, packet: protocol.packet.Packet):
+        if packet_type is protocol.CLIENTBOUND_EventUserJoined:
+            for user in self._userlist:
+                if user != packet.user:
+                    continue
+                return
+            self._userlist.append(packet.user)
+            return
+        if packet_type is protocol.CLIENTBOUND_EventUserLeft:
+            for user in self._userlist:
+                if user != packet.user:
+                    continue
+                self._userlist.remove(packet.user)
+                return
+            return
+        if packet_type is protocol.CLIENTBOUND_ServerMessage:
+            return await self._invoke_event("on_server_message", packet.message)
+        if packet_type is protocol.CLIENTBOUND_UserMessage:
+            return await self._invoke_event("on_message", self.find_user_by_fingerprint(packet.fingerprint), packet.message)
+        
+        
+    def find_user_by_fingerprint(self, fingerprint: str) -> primitives.User|None:
+        for user in self._userlist:
+            if user.fingerprint != fingerprint:
+                continue
+            return user
+        return None
+        
