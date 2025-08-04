582cc8b2227a3148255b4a2e15600594d8cc41df
googer <googer@mindurka.fun>
2025-08-04 22:29:39 +0700

    implement client functionality

--- client_gui_new/client.py
+++ client_gui_new/client.py
@@ -59,17 +59,27 @@ class ChannelSidebar(customtkinter.CTkScrollableFrame):
         self.command = channel_select
         self._channels_list = []
         self._channels_lookup = {}
-        self.add_channel(-1, "logs")
+        self.add_channel(-1, "console")
 
-    def add_channel(self, channel_id: int, name: str):
+    def add_channel(self, channel_id: int, name: str) -> bool:
+        if self._channels_lookup.get(channel_id, None) is not None: return False
         label = customtkinter.CTkButton(self,
             text=name,
             command=lambda channel_id=channel_id: self._select_channel(channel_id),
             fg_color="transparent",
             anchor="w")
         label.grid(row=len(self._channels_list), column=0, pady=(0, 5), sticky="nswe")
-        self._channels_lookup[len(self._channels_list)] = label
         self._channels_list.append(label)
+        self._channels_lookup[channel_id] = label
+        return True
+
+    def remove_channel(self, channel_id: int) -> bool:
+        if self._channels_lookup.get(channel_id, None) is None: return False
+        label = self._channels_lookup[channel_id]
+        label.grid_forget()
+        label.destroy()
+        del label
+        # self.reorganise()
 
 
     def _select_channel(self, channel_id):
@@ -84,6 +94,9 @@ class UserPanel(customtkinter.CTkFrame):
             text=settings.name)
         self.name_label.grid(row=0, column=0, sticky="nswe")
 
+    def set_server_name(self, new_name: str|None = None):
+        self.name_label.configure(text=new_name if new_name is not None else settings.name)
+
 class MessageFrame(customtkinter.CTkFrame):
     name_label: customtkinter.CTkLabel
     content_label: customtkinter.CTkLabel
@@ -95,6 +108,7 @@ class MessageFrame(customtkinter.CTkFrame):
         self.name_label = customtkinter.CTkLabel(self,
             text=name,
             anchor="w",
+            justify="left",
             font=customtkinter.CTkFont(size=14, weight="bold"))
         self.name_label.grid(row=0, column=0, sticky="nswe", padx=(10, 3), pady=(3, 3))
         self.content_label = customtkinter.CTkLabel(self,
@@ -113,7 +127,7 @@ class MessagesFrame(customtkinter.CTkScrollableFrame):
     def add_message(self, name: str, content: str):
         print(f"<{name}> {content}")
         message = MessageFrame(self, name, content)
-        message.grid(row=len(self.messages), column=0, sticky="nswe")
+        message.grid(row=len(self.messages), column=0, sticky="nswe", pady=(0, 5))
         self.messages.append(message)
 
 
@@ -122,18 +136,25 @@ class ChatFrame(customtkinter.CTkFrame):
     message_entry: customtkinter.CTkEntry
     current_channel: int|None
 
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, on_enter, **kwargs):
         super().__init__(master, **kwargs)
+        self.command = on_enter
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.message_entry = customtkinter.CTkEntry(self, placeholder_text="Message...")
         self.message_entry.grid(row=1, column=0, sticky="nswe")
+        self.message_entry.bind("<Return>", lambda x: self._enter())
         self.messages_frames = {}
         self.current_channel = None
 
         self.add_channel(-1)
         self.select_channel(-1)
-        self.add_message(-1, "SYSTEM", "This channel will contain client logs")
+        self.add_message(-1, "SYSTEM", "This channel will contain client logs and can be used to run client commands")
+
+    def _enter(self):
+        text = self.message_entry.get()
+        self.message_entry.delete(0, len(text)+1)
+        self.command(text)
 
     def select_channel(self, channel_id: int) -> bool:
         if self.current_channel is not None and self.messages_frames.get(self.current_channel, None):
@@ -146,9 +167,18 @@ class ChatFrame(customtkinter.CTkFrame):
         return True
 
     def add_channel(self, channel_id: int) -> bool:
-        if self.messages_frames.get(channel_id, None): return False
+        if self.messages_frames.get(channel_id, None) is not None: return False
         messages_frame = MessagesFrame(self)
         self.messages_frames[channel_id] = messages_frame
+        return True
+
+    def remove_channel(self, channel_id: int) -> bool:
+        if channel_id == -1: return False
+        if self.messages_frames.get(channel_id, None) is None: return False
+        if channel_id == self.current_channel:
+            self.select_channel(-1)
+        self.messages_frames[channel_id].destroy()
+        del self.messages_frames[channel_id]
         return True
 
     def add_message(self, channel_id: int, name: str, content: str) -> bool:
@@ -163,15 +193,18 @@ class MainWindow(customtkinter.CTk):
     userpanel: UserPanel = None
     chat_frame: ChatFrame = None
 
-    loop = None
-    tasks = None
+    loop: asyncio.EventLoop = None
+    tasks: list = None
+    channels: dict[int, str]
+    current_channel: int = -1
 
     def __init__(self):
         super().__init__()
 
         self.loop = asyncio.get_event_loop()
         self.tasks = []
-        self.protocol("WM_DELETE_WINDOW", self.close)
+        self.channels = {}
+        self.protocol("WM_DELETE_WINDOW", self._close)
 
         self.title("Scalar")
         self.geometry("800x600")
@@ -184,19 +217,47 @@ class MainWindow(customtkinter.CTk):
         self.userpanel = UserPanel(self)
         self.userpanel.grid(row=1, column=0, sticky="we")
 
-        self.chat_frame = ChatFrame(self)
+        self.chat_frame = ChatFrame(self, self.send_message)
         self.chat_frame.grid(row=0, column=1, rowspan=4, sticky="nswe")
-
-        self.add_channel(0, "testing")
 
         for method in dir(self):
             if not method.startswith("_client_event__"): continue
             self.client.event(method[15:])(getattr(self, method))
 
-    def add_channel(self, channel_id: int, name: str):
+    def add_channel(self, channel_id: int, name: str) -> bool:
+        if self.channels.get(channel_id, None) is not None: return False
         self.channel_bar.add_channel(channel_id, name)
         self.chat_frame.add_channel(channel_id)
+        self.channels[channel_id] = name
+        return True
+
+    def remove_channel(self, channel_id: int) -> bool:
+        if self.channels.get(channel_id, None) is None: return False
+        self.channel_bar.remove_channel(channel_id)
+        self.chat_frame.remove_channel(channel_id)
+        del self.channels[channel_id]
+        return True
+
+    def send_message(self, content):
+        if self.current_channel == -1:
+            return self._process_console(content)
+        chan = self.client.find_channel(self.current_channel)
+        if not chan: return end
+        self.loop.create_task(self.client.send_message(chan, content))
+
+    def _process_console(self, uinput):
+        command = uinput[:uinput.index(" ")] if " " in uinput else uinput
+        uinput = uinput[len(command)+1:]
+        if command == "who":
+            if not self.client.connected():
+                return self.add_message(-1, "CLIENT", "Not connected, command unavailable.")
+            reply = "Connected users:"
+            for user in self.client._userlist:
+                reply += f"\n    {user.username} / fingerprint={user.fingerprint:16x}"
+            return self.add_message(-1, "CLIENT", reply)
+
     def select_channel(self, channel_id: int):
+        self.current_channel = channel_id
         return self.chat_frame.select_channel(channel_id)
     def add_message(self, channel_id: int, name: str, content: str):
         return self.chat_frame.add_message(channel_id, name, content)
@@ -214,11 +275,34 @@ class MainWindow(customtkinter.CTk):
             self.update()
             await asyncio.sleep(interval)
 
-    def close(self):
+    def _close(self):
         for task in self.tasks:
             task.cancel()
         self.loop.stop()
         self.destroy()
 
+    def _reset(self):
+        self.userpanel.set_server_name(None)
+        for channel_id in self.channels.keys():
+            self.remove_channel(channel_id)
+
+    def _client_event__on_socket_broken(self, client):
+        self._reset()
+
     def _client_event__on_login_complete(self, client):
         self.add_message(-1, "CLIENT", "Logged in!")
+    def _client_event__on_uinfo_negotiated(self, client, username):
+        self.add_message(-1, "CLIENT", "User info negotiated")
+        self.userpanel.set_server_name(username)
+    def _client_event__on_channellist_received(self, client):
+        for channel in client._channellist:
+            self.add_channel(channel.cid, channel.name)
+
+    def _client_event__on_user_joined(self, client, user):
+        self.add_message(-1, "SERVER", f"{user.username} (fingerprint={user.fingerprint:16x}) joined")
+    def _client_event__on_user_left(self, client, user):
+        self.add_message(-1, "SERVER", f"{user.username} (fingerprint={user.fingerprint:16x}) left")
+    def _client_event__on_message(self, client, message):
+        self.add_message(message.channel.cid, f"<{message.author.username}>", message.content)
+    def _client_event__on_server_message(self, client, message):
+        self.add_message(message.channel.cid, "[SERVER]", message.content)
